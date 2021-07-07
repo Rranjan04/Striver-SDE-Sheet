ffa24165e56a777c97370d28da671dbd04c7d38d
Ritika Ranjan <rranjan1922@gmail.com>
2021-07-07 22:19:16 +0530

    Day1

--- Day01/KadanesAlgorithm.java
+++ Day01/KadanesAlgorithm.java
@@ -1,5 +1,39 @@
 package Day01;
 
 public class KadanesAlgorithm {
-	
+	public static void main(String[] args) {
+		
+	}
+	static int kadanes(int[] arr){
+		int sum = 0 , maxSoFar = Integer.MIN_VALUE;
+		for(int x:arr){
+			sum+=x;
+			if(sum<x){
+				sum = x;
+			}
+			if(sum>maxSoFar){
+				maxSoFar = sum;
+			}
+		}
+		return maxSoFar;
+	}
+	static int kadanesAndIndex(int[] arr){
+		int sum = 0, maxSoFar = Integer.MIN_VALUE;
+		int end = 0, start =0 ,s=0;
+		for(int i=0;i<arr.length;i++){
+			sum+=arr[i];
+			if(maxSoFar<sum){
+				maxSoFar = sum;
+				start = s;
+				end = i;
+			}
+			if(sum<arr[i]){
+				sum = arr[i];
+				s = i;
+			}
+		}
+		System.out.println(start);
+		System.out.println(end);
+		return maxSoFar;
+	}
 }
--- Day01/MergeOverlappingSubIntervals.java
+++ Day01/MergeOverlappingSubIntervals.java
@@ -1,5 +1,42 @@
 package Day01;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Stack;
+
 public class MergeOverlappingSubIntervals {
-	
+	static class Time{
+		int start;
+		int end;
+		Time(int start, int end){
+			this.start = start;
+			this.end = end;
+		}
+		
+	}
+	static ArrayList<Time> merge(ArrayList<Time> intervals){
+		Stack<Time> st = new Stack<>();
+		Collections.sort(intervals,new Comparator<Time>(){
+			public int compare(Time t1, Time t2){
+				return t1.start- t2.start;
+			}
+		});
+		st.push(intervals.get(0));
+		for(int i=1;i<intervals.size();i++){
+			Time curr = st.peek();
+			if(curr.end<intervals.get(i).start){
+				st.push(intervals.get(i));
+				continue;
+			}
+			else if(curr.end>=intervals.get(i).start){
+				curr.end = Math.max(curr.end, intervals.get(i).end);
+			}
+		}
+		ArrayList<Time> ans = new ArrayList<>();
+		while(!st.isEmpty()){
+			ans.add(st.pop());
+		}
+		return ans;
+	}
 }
