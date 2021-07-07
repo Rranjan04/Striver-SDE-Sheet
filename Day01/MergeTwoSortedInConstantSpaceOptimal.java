4d58b4c3f9e36ad92aa5d3d5e35e89ec3621bbf7
Ritika Ranjan <rranjan1922@gmail.com>
2021-07-07 21:30:06 +0530

    Day01

--- Day01/MergeTwoSortedInConstantSpaceOptimal.java
+++ Day01/MergeTwoSortedInConstantSpaceOptimal.java
@@ -4,4 +4,23 @@ public class MergeTwoSortedInConstantSpaceOptimal {
 	public static void main(String[] args) {
 		
 	}
+	static void merge(int[] a1,int[] a2){
+		int m = a1.length;
+		int n = a2.length;
+		for(int i=0;i<m;i++){
+			if(a1[i]>a2[0]){
+				//swap
+				int temp = a1[i];
+				a1[i] = a2[0];
+				a2[0] = temp;
+				//move y0 to correct position
+				int key = a2[0];
+				int j;
+				for(j=1;j<n && a2[j]<a2[0];j++){
+					a2[j-1] = a2[j];
+				}
+				a2[j-1] = key;
+			}
+		}
+	}
 }
