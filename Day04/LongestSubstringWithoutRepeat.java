6e62fe2df9964a8725fe436dc591bfd9bc52bdc4
Ritika Ranjan <rranjan1922@gmail.com>
2021-07-11 11:04:51 +0530

    Day4

--- Day04/LongestSubstringWithoutRepeat.java
+++ Day04/LongestSubstringWithoutRepeat.java
@@ -1,5 +1,6 @@
 package Day04;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class LongestSubstringWithoutRepeat {
@@ -7,6 +8,8 @@ public class LongestSubstringWithoutRepeat {
 		
 	}
 	public int lengthOfLongestSubstring1(String s) {
+		// Sliding Window
+		// Worst : O(2*n)
         HashSet<Character> set = new HashSet<>();
         int l = 0, r=0;
         int count =0 ;
@@ -22,6 +25,23 @@ public class LongestSubstringWithoutRepeat {
             }
             set.add(ch);
             count = Math.max(count,set.size());
+            r++;
+        }
+        return count;
+    }
+	public int lengthOfLongestSubstringII(String s) {
+        if(s.length()==1||s.length()==0) return s.length();
+        int[] arr = new int[256];
+        Arrays.fill(arr,-1);
+        int r =0,l=0;
+        int count =0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(arr[ch]!=-1){
+                l = Math.max(l,arr[ch]+1);
+            }
+            arr[ch] = r;
+            count = Math.max(r-l+1,count);
             r++;
         }
         return count;
