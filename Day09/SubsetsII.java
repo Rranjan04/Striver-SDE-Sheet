17cad21a1f745a9427dbbc479e7e47ce96b4c8ff
Ritika Ranjan <rranjan1922@gmail.com>
2021-07-12 19:52:13 +0530

    Day 09

---
+++ Day09/SubsetsII.java
@@ -0,0 +1,22 @@
+package Day09;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public  class SubsetsII {
+	public List<List<Integer>> findSubsets(int[] nums){
+		Arrays.sort(nums);
+		List<List<Integer>> ans = new ArrayList<>();
+		help(0,nums,new ArrayList<>(),ans);
+		return ans;
+	}
+	public void help(int idx, int[] nums, ArrayList<Integer> ds,List<List<Integer>> ans){
+		ans.add(new ArrayList<>(ds));
+		for(int i=idx;i<nums.length;i++){
+			if(i!=idx && nums[i]==nums[i-1]) continue;
+			ds.add(nums[i])l
+			help(i+1,nums,ds,ans);
+		}
+	}
+}
