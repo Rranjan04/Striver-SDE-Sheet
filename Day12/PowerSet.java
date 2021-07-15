f8905702af17230d4aa6df808928f9795caeac8b
Ritika Ranjan <rranjan1922@gmail.com>
2021-07-15 15:41:40 +0530

    Day 12

--- Day12/PowerSet.java
+++ Day12/PowerSet.java
@@ -1,5 +1,22 @@
 package Day12;
 
+import java.util.ArrayList;
+
 public class PowerSet {
-	
+	static ArrayList<String> generate(String s){
+		int n = s.length();
+		ArrayList<String> ans = new ArrayList<>();
+		for(int num = 0;num<=(1<<n)-1;num++){
+			// num<2^n-1
+			String sub ="";
+			for(int i=0;i<n;i++){
+				if((num & (1<<i))==1){
+					// checking set bit
+					sub+=s.charAt(i);
+				}
+			}
+			ans.add(sub);
+		}
+		return ans;
+	}
 }
