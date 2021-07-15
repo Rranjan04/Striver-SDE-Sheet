008f7b9f1bced9a01522bd89ee1cfb41ff343416
Ritika Ranjan <rranjan1922@gmail.com>
2021-07-15 09:05:41 +0530

    Day 11

--- Day11/Pivot.java
+++ Day11/Pivot.java
@@ -1,5 +1,31 @@
 package Day11;
 
 public class Pivot {
-	
+	public int search(int[] arr, int target) {
+		//         2 cases : left half is sorted OR right half is sorted
+				int low =0;
+				int high = arr.length-1;
+				while(low<=high){
+					int mid = low+(high-low)/2;
+					if(arr[mid]==target){
+						return mid;
+					}else if(arr[mid]>=arr[low]){
+						//left array is sorted
+						if(target>=arr[low] && target<arr[mid]){
+							//target lies in sorted array
+							high = mid-1;
+						}else{
+							//target lies in the right array
+							low = mid+1;
+						}
+					}else{
+						if(target>arr[mid] && target<=arr[high]){
+							low = mid+1;
+						}else{
+							high = mid-1;
+						}
+					}
+				}
+				return -1;
+			}
 }
