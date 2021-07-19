a55c99335a5d02c09d847fe338fc4cf871bd1d82
Ritika Ranjan <rranjan1922@gmail.com>
2021-07-19 18:43:28 +0530

    Day 22

--- Day22/DistinctNumbers.java
+++ Day22/DistinctNumbers.java
@@ -1,5 +1,34 @@
 package Day22;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class DistinctNumbers {
-	
+	public int[] maxSlidingWindow(int[] arr, int k) {
+        int[] ans = new int[arr.length-k+1];
+        Deque<Integer> dq = new LinkedList<>();
+        int n = arr.length;
+        int i=0;
+        for(;i<k;i++){
+            while(!dq.isEmpty() && arr[dq.peekLast()]<arr[i]){
+                dq.removeLast();
+            }
+            dq.addLast(i);
+        }
+        for(;i<n;i++){
+            ans[i-k] = arr[dq.peekFirst()];
+//             removing elements from previous window
+            while(!dq.isEmpty() && dq.peekFirst()<=i-k){
+                dq.removeFirst();
+            }
+//             adding new elemets
+            while(!dq.isEmpty() && arr[dq.peekLast()]<arr[i]){
+                dq.removeLast();
+            }
+            dq.addLast(i);
+        }
+//         last window is yet to be updated
+        ans[i-k] = arr[dq.peekFirst()];
+        return ans;
+    }
 }
