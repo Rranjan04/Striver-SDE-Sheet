0954b1f43cc60cb4a2e14d812b78d650f9de8bff
Ritika Ranjan <rranjan1922@gmail.com>
2021-07-20 15:22:02 +0530

    Day 23

--- Day23/Clone.java
+++ Day23/Clone.java
@@ -1,5 +1,46 @@
 package Day23;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
 public class Clone {
-	
+	public Node clone(Node node){
+		HashMap<Node,Node> map = new HashMap<>();
+		Queue<Node> q = new LinkedList<>();
+		q.add(node);
+		map.put(node, new Node(node.val,new ArrayList<>()));
+		while(!q.isEmpty()){
+			Node top = q.poll();
+			for(Node n:top.neighbors){
+				// put neighbours in map
+				if(!map.containsKey(n)){
+					map.put(n, new Node(n.val,new ArrayList<>()));
+					q.add(n);
+				}
+				map.get(top).neighbors.add(map.get(n));
+			}
+
+		}
+		return map.get(node);
+	}
 }
--- Day23/NoOfIslands.java
+++ Day23/NoOfIslands.java
@@ -1,5 +1,26 @@
 package Day23;
 
 public class NoOfIslands {
-	
+	// In grid
+	public int numIslands(char[][] grid) {
+        int count = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(!visited[i][j] && grid[i][j]=='1'){
+                    dfs(grid,i,j,visited);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public void dfs(char[][] grid, int i, int j, boolean[][] visited){
+        if(i>=grid.length||j>=grid[0].length||i<0||j<0||visited[i][j]||grid[i][j]=='0') return;
+        visited[i][j] = true;
+        dfs(grid,i+1,j,visited);
+        dfs(grid,i-1,j,visited);
+        dfs(grid,i,j-1,visited);
+        dfs(grid,i,j+1,visited);
+    }
 }
